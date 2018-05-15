8efcd5c2a84384db12d3c3a4593356399c727a6e
github username <github email address>
2018-05-15 18:00:23 -0400

    finished lab

--- bin/run.rb
+++ bin/run.rb
@@ -5,4 +5,5 @@ require_relative "../lib/command_line_interface.rb"
 
 welcome
 character = get_character_from_user
+puts "The Movies for #{character} are:"
 show_character_movies(character)
--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -3,10 +3,7 @@ require 'json'
 require 'pry'
 
 def get_character_movies_from_api(character)
-  #make the web request
-  all_characters = RestClient.get('http://www.swapi.co/api/people/')
-  character_hash = JSON.parse(all_characters)
-  
+
   # iterate over the character hash to find the collection of `films` for the given
   #   `character`
   # collect those film API urls, make a web request to each URL to get the info
@@ -16,16 +13,43 @@ def get_character_movies_from_api(character)
   # this collection will be the argument given to `parse_character_movies`
   #  and that method will do some nice presentation stuff: puts out a list
   #  of movies by title. play around with puts out other info about a given film.
+
+  all_characters = RestClient.get('http://www.swapi.co/api/people/') #make the web request
+  character_hash = JSON.parse(all_characters)
+  character_hash["results"].each do |character_h|
+    if character_h["name"] == character
+      return character_h["films"].collect do |urls|
+
+        movies = RestClient.get urls
+        movies_hash = JSON.parse(movies)
+        movies_hash
+      end
+    end
+  end
 end
 
 def parse_character_movies(films_hash)
   # some iteration magic and puts out the movies in a nice list
+  films_hash.each_with_index do |movies,i|
+    puts "#{i+1}. #{movies["title"]}"
+  end
+
+end
+
+def x(film)
+  films_hash.collect do |urls|
+    movies = RestClient.get urls
+    movies_hash = JSON.parse(movies)
+    movies_hash
+  end
 end
 
 def show_character_movies(character)
   films_hash = get_character_movies_from_api(character)
   parse_character_movies(films_hash)
 end
+
+# Pry.start
 
 ## BONUS
 
